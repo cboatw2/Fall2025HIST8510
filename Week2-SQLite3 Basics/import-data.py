5bdbb32beb0cc3be1ced6cc9444e5137efe4c396
Candy Rae Boatwright <cboatw2@clemson.edu>
2025-09-08 16:48:39 -0400

    Week 2 and 3

--- Week2-SQLite3 Basics/import-data.py
+++ Week2-SQLite3 Basics/import-data.py
@@ -18,7 +18,7 @@ print("\nStep 2: Reading CSV file...")
 venues_data = []
 
 try:
-    with open('corand100.csv', 'r', encoding='utf-8') as file:
+    with open('co-rand-100.csv', 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             venues_data.append(row)
